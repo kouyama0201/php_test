f86617e5c1d6dd259f1431ab2c6124e441580468
kouyama0201 <kami19900201@gmail.com>
2022-10-25 19:39:38 +0900

    37. フォームセキュリティ XSS

--- form/input.php
+++ form/input.php
@@ -1,24 +1,31 @@
 <?php
 
-if(!empty($_POST)) {
-  echo $_POST;
-}
-// スーパーグローバル変数 php 9種類
-// 連想配列
+  header('X-FRAME-OPTIONS:DENY');
 
+  // スーパーグローバル変数 php 9種類
+  // 連想配列
+  // if(!empty($_POST)) {
+  //   echo '<pre>';
+  //   var_dump($_POST);
+  //   echo '</pre>';
+  // }
 
-// 入力、確認、完了 input.php, confirm.php, thanks.php
-// input.php
+  function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+  }
 
-$pageFlag = 0;
+  // 入力、確認、完了 input.php, confirm.php, thanks.php
+  // input.php
 
-if(!empty($_POST['btn_confirm'])) {
-  $pageFlag = 1;
-}
+  $pageFlag = 0;
 
-if(!empty($_POST['btn_submit'])) {
-  $pageFlag = 2;
-}
+  if(!empty($_POST['btn_confirm'])) {
+    $pageFlag = 1;
+  }
+
+  if(!empty($_POST['btn_submit'])) {
+    $pageFlag = 2;
+  }
 
 ?>
 
@@ -32,10 +39,10 @@ if(!empty($_POST['btn_submit'])) {
     <?php if($pageFlag === 0): ?>
       <form method="POST" action="input.php">
         氏名
-        <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'];} ?>">
+        <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo h($_POST['your_name']);} ?>">
         <br>
         メールアドレス
-        <input type="email" name="email"  value="<?php if(!empty($_POST['email'])){echo $_POST['email'];} ?>">
+        <input type="email" name="email"  value="<?php if(!empty($_POST['email'])){echo h($_POST['email']);} ?>">
         <br>
         <input type="submit" name="btn_confirm" value="確認する">
       </form>
@@ -44,16 +51,16 @@ if(!empty($_POST['btn_submit'])) {
     <?php if($pageFlag === 1): ?>
       <form method="POST" action="input.php">
         氏名
-        <?php echo $_POST['your_name']; ?>
+        <?php echo h($_POST['your_name']); ?>
         <br>
         メールアドレス
-        <?php echo $_POST['email']; ?>
+        <?php echo h($_POST['email']); ?>
         <br>
         <input type="submit" name="back" value="戻る">
         <input type="submit" name="btn_submit" value="送信する">
 
-        <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+        <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+        <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
       </form>
     <?php endif; ?>
 
