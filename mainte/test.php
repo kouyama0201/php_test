dad59a0c54df71125ba19e12f2b8f957417cb31a
kouyama0201 <kami19900201@gmail.com>
2022-10-27 20:01:58 +0900

    46. ファイル操作 その1

--- mainte/test.php
+++ mainte/test.php
@@ -1,7 +1,33 @@
-<?
-// パスワードを記録したファイルの場所
-echo __FILE__;
+<?php
+  // パスワードを記録したファイルの場所
+  echo __FILE__;
 
-echo '<br>';
-// パスワード(暗号化)
-echo(password_hash('password123', PASSWORD_BCRYPT));
+  echo '<br>';
+  // パスワード(暗号化)
+  echo(password_hash('password123', PASSWORD_BCRYPT));
+
+  $contactFile = '.contact.dat';
+
+  // ファイル丸ごと読み込み
+  $fileContents = file_get_contents($contactFile);
+
+  echo $fileContents;
+
+  // ファイルに書き込み(上書き)
+  file_put_contents($contactFile, 'テストです');
+
+  $addText = 'テストです' . "\n"; // 改行
+
+  // ファイルに書き込み(追記)
+  file_put_contents($contactFile, $addText, FILE_APPEND);
+
+  // 配列 file ,区切る explode, foreach
+  $allData = file($contactFile);
+
+  foreach($allData as $lineData) {
+    $lines = explode(',', $lineData);
+    echo $lines[0]. '<br>';
+    echo $lines[1]. '<br>';
+    echo $lines[2]. '<br>';
+  }
+?>
