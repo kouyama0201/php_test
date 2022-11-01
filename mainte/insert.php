972021ee241e8f365f3d01869af67fca1ad55307
kouyama0201 <kami19900201@gmail.com>
2022-11-01 19:48:36 +0900

    56. フォーム値をDBに保存 その2

--- mainte/insert.php
+++ mainte/insert.php
@@ -1,37 +1,49 @@
 <?php
+  function insertContact($request) {
+    // DB接続 PDO
+    require 'db_connection.php'
+  
+    // 入力 DB保存 prepare, bind, execute(配列(全て文字列))
+    $params = [
+      'id' => null,
+      'your_name' => $request['your_name'],
+      'email' => $request['email'],
+      'url' => $request['url'],
+      'gender' => $request['gender'],
+      'age' => $request['age'],
+      'contact' => $request['contact'],
+      'created_at' => null,
+    ];
 
-  // DB接続 PDO
-  require 'db_connection.php'
+    // $params = [
+    //   'id' => null,
+    //   'your_name' => 'なまえ',
+    //   'email' => 'test@test.com',
+    //   'url' => 'http://test.com',
+    //   'gender' => '1',
+    //   'age' => '2',
+    //   'contact' => 'いいい',
+    //   'created_at' => null,
+    // ];
 
-  // 入力 DB保存 prepare, bind, execute(配列(全て文字列))
-  $params = [
-    'id' => null,
-    'your_name' => 'なまえ',
-    'email' => 'test@test.com',
-    'url' => 'http://test.com',
-    'gender' => '1',
-    'age' => '2',
-    'contact' => 'いいい',
-    'created_at' => null,
-  ];
+    $count = 0;
+    $columns = '';
+    $values = '';
 
-  $count = 0;
-  $columns = '';
-  $values = '';
-
-  foreach(array_keys($params) as $key) {
-    if($count++ > 0) {
-      $columns .= ',';
-      $values .= ',';
+    foreach(array_keys($params) as $key) {
+      if($count++ > 0) {
+        $columns .= ',';
+        $values .= ',';
+      }
+      $columns .= $key;
+      $values .= ':'.$key;
     }
-    $columns .= $key;
-    $values .= ':'.$key;
+
+    $sql = 'insert into contacts ('.$columns.')values('.$values.')';
+    // var_dump($sql);
+    // exit;
+
+    $stmt = $pdo->prepare($sql); // プリペアードステートメント
+    $stmt->execute($params); // 実行
   }
-
-  $sql = 'insert into contacts ('.$columns.')values('.$values.')';
-  // var_dump($sql);
-  // exit;
-
-  $stmt = $pdo->prepare($sql); // プリペアードステートメント
-  $stmt->execute($params); // 実行
 ?>
